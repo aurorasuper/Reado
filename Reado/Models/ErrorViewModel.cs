7cf3a7bbfb5d7beb9b16a73b7b3b43739db6dab9
Susan Kronberg <susskronberg@gmail.com>
2023-02-23 15:32:50 +0100

    "deleted copy of old solution"

--- Reado/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Reado.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
